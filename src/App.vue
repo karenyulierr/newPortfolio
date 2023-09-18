663a6c46598736462cc1124571d17cac2985601b
Karen Rodriguez <kyrodriguez@gmail.com>
2023-09-18 15:46:09 -0500

    Ajustes diseño y pagina WhoIAm

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
-    <Header/>
-    <Footer/>
+
  <RouterView />
 </template>
 <style>
